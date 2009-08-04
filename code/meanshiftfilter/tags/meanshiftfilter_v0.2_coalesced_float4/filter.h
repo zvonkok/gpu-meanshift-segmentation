b4cb2a62dab248c13d27c06bbabeabb25fb65aa2
zvonkok <zvonkok@66dbd094-af6f-0410-9a11-29bee85f8b20>
2009-08-04 18:49:39 +0000

    added tag float4 datatype, coalesced 128byte global mem reads

---
+++ code/meanshiftfilter/tags/meanshiftfilter_v0.2_coalesced_float4/filter.h
@@ -0,0 +1,35 @@
+/*
+ *  filter.h
+ *  MeanShift
+ *
+ *  Created by Zvonko Krnjajic on 23.07.09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef FILTER_H
+#define FILTER_H
+
+#include <cuda_runtime_api.h>
+
+const float	EPSILON	= 0.01f;	// define threshold (approx. Value of Mh at a peak or plateau)
+const float	LIMIT   = 100.0f;	// define max. # of iterations to find mode
+
+
+extern "C" void Filter();
+
+extern unsigned int * h_filt;
+extern float4 * h_src;
+extern float4 * h_dst;
+
+extern unsigned int height;
+extern unsigned int width;
+
+extern unsigned int N;
+extern unsigned int L;
+
+extern float sigmaS;
+extern float sigmaR;
+
+#endif // FILTER_H
+
